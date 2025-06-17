982132533d8338597f340fa0a831c58102678e41
Aman sagar <145146325+aman743sagar@users.noreply.github.com>
2025-06-17 21:21:54 +0530

    Update VideoMeet.jsx

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -334,7 +334,7 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 import ChatIcon from '@mui/icons-material/Chat';
 
 
-const server_url = "http://localhost:8000"
+const server_url = "https://videocall-i6i5.onrender.com"
 
 const connections = {};
 
